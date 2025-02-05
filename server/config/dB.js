7040c465de1b462269da0b4f16ed1d46b351ba9a
Yeniffer8a <jennyov19@hotmail.com>
2025-02-05 17:35:38 -0500

    Add server file

---
+++ server/config/dB.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import "dotenv/config";
+
+const mongoUrl= process.env.MONGO_URL
+
+async function connectDB() {
+    try{
+        await mongoose.connect(mongoUrl)
+        console.log('database connected')
+    } catch (error){
+        throw new Error (`Error connect the Data Base: ${error.messaga}`)
+    }
+}
+
+export default connectDB
---
+++ server/index.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import "dotenv/config";
+import connectDB from './config/dB.js';
+import apiRouter from './routes/apiRouter.js'
+
+const app = express()
+
+app.use(express.json());
+
+
+connectDB();
+
+app.use("",apiRouter);
+
+const port = process.env.PORT;
+
+app.listen(port,()=>{
+    console.log(`the server is running on the port: ${port}`)
+})
