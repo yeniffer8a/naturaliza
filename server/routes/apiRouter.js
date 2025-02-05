35a765b89ccd44af1511f81ff927c0d4596038dd
Yeniffer8a <jennyov19@hotmail.com>
2025-02-05 18:36:42 -0500

    Add user, routes

--- server/routes/apiRouter.js
+++ server/routes/apiRouter.js
@@ -1,10 +1,10 @@
 import express from "express";
-
-
-
+import userRoutes from "./userRoutes.js";
+import authRoutes from "./authRoutes.js";
 
 const router = express.Router();
 
+router.use("/api", authRoutes);
+router.use("/api", userRoutes);
 
-
-export default router
+export default router;
