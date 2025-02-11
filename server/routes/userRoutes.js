568df54776ecbfded9005c24f7d2e049b1eeb8a2
Yeniffer8a <jennyov19@hotmail.com>
2025-02-11 11:20:04 -0500

    Save to make merge

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import { createUser } from "../controllers/userController.js";
-import { expressjwt } from "express-jwt";
+//import { expressjwt } from "express-jwt";
 
 dotenv.config();
 const router = express.Router();
