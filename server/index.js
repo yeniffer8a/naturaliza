2a377a5d95d762540c845defc4e7e36346855f18
Yeniffer8a <jennyov19@hotmail.com>
2025-02-14 15:23:02 -0500

    add cors library

--- server/index.js
+++ server/index.js
@@ -2,11 +2,11 @@ import express from "express";
 import "dotenv/config";
 import connectDB from "./config/dB.js";
 import apiRouter from "./routes/apiRouter.js";
-
+import cors from "cors";
 const app = express();
 
 app.use(express.json());
-
+app.use(cors());
 connectDB();
 
 app.use("", apiRouter);
