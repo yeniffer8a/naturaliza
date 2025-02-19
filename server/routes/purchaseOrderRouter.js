6c81767c952a943c8e1474d5fb49f9b32c971e09
Luis.Lozano <>
2025-02-18 22:13:01 -0500

    end endpoint update

--- server/routes/purchaseOrderRouter.js
+++ server/routes/purchaseOrderRouter.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { listPurchaseOrders,createNewPurchaseOrder,deleteOrderByNumber } from "../controllers/purchaseOrdenController.js";
+import { listPurchaseOrders,createNewPurchaseOrder,deleteOrderByNumber,updateStatus } from "../controllers/purchaseOrdenController.js";
 import { purchaseToken } from "../middleware/purchaseToken.js";
 import {tokenValidator} from "../middleware/tokenValidator.js";
 import {rolUserValidator} from "../middleware/rolUserValidator.js"
@@ -12,6 +12,8 @@ const router = express.Router()
 router.get("/listPurchase",listPurchaseOrders);
 
 router.post("/createorder",purchaseToken,createNewPurchaseOrder)
+
+router.patch("/updatestatus",updateStatus)
 
 router.delete("/deleteorder/:ordernumber",tokenValidator,expressjwt({secret:process.env.JWT_SECRET, algorithms: ['HS256']}),rolUserValidator,deleteOrderByNumber)
 
